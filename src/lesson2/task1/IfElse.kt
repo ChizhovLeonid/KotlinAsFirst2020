11a377110b6e6acd12538cd1cec1a20fa1df5226
ChizhovLeonid <chigov89@yandex.ru>
2020-09-13 20:16:32 +0300

    Error fix

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -203,6 +203,7 @@ fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
         (!(a < c) && (b < d)) -> b - a
         (!(a < c) && !(b < d) && (a < d)) -> d - a
         ((a < c) && !(b < d)) -> d - c
+        ((a == b) && (a == c) && (a == d)) -> 0
         else -> -1
     }
 }
